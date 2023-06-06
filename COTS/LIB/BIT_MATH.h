8db5f92ccb00aa41c1847579019a360d5b7afdc5
Tarek <tareksabah208@gmail.com>
2023-06-06 07:46:33 +0300

    LIB Folder

---
+++ COTS/LIB/BIT_MATH.h
@@ -0,0 +1,12 @@
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+
+#define SET_BIT(REG,BIT)        (REG |= (1 << BIT))
+
+#define CLEAR_BIT(REG,BIT)      (REG &= ~(1 << BIT))
+
+#define GET_BIT(REG,BIT)        ((REG >> BIT)&1)
+
+#define TOGGLE_BIT(REG,BIT)     (REG ^= (1 << BIT))
+
+#endif
